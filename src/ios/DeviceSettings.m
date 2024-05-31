b96913c32e01159e7f2c8a2842beaa943d946e81
Vagish M M <vagish.muralidhara@sofbang.com>
2024-05-31 12:42:49 +0530

    first commit

---
+++ src/ios/DeviceSettings.m
@@ -0,0 +1,40 @@
+#import "DeviceSettings.h"
+
+@implementation DeviceSettings
+
+- (BOOL)do_open:(NSString *)pref {
+	if ([[UIApplication sharedApplication] openURL:[NSURL URLWithString:pref]]) {
+		return YES;
+	} else {
+		return NO;
+	}
+}
+
+- (void)open:(CDVInvokedUrlCommand*)command
+{
+	CDVPluginResult* pluginResult = nil;
+	NSString* key = [command.arguments objectAtIndex:0];
+	NSString* prefix = @"App-Prefs:";
+	BOOL result = NO;
+
+	if(SYSTEM_VERSION_LESS_THAN(@"17.0")){
+		prefix = @"prefs:";
+	}
+	
+    if ([key isEqualToString:@"settings"]) {
+		result = [self do_open:prefix];
+	}	
+	else {
+		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Invalid Action"];
+	}
+		
+	if (result) {
+		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Opened"];
+	} else {
+		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Cannot open"];
+	}
+	
+	[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
---
+++ www/settings.js
@@ -0,0 +1,9 @@
+var DeviceSettings = function() {
+};
+
+DeviceSettingsSettings.open = function(setting, onsucess, onfail) {
+	var settings = (typeof setting === 'string' || setting instanceof String) ? [setting] : setting;
+	cordova.exec(onsucess, onfail, "DeviceSettings", "open", settings);
+};
+
+module.exports = DeviceSettings;
